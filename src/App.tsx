0a442bf8b923eb6334a21fefe7cf50273a4050fc
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-22 12:53:29 +0530

    Adding Tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
-// import './App.css'
+import { useState } from "react";
 
 type Priority = "p1" | "p2" | "p3";
 type Task = {
-  id: number;
-  title: string;
-  isCompleted: boolean;
-  priority?: Priority;
+  id: number,
+  title: string,
+  isCompleted: boolean,
+  priority?: Priority
 };
 
-function App() {
-  const task: Task[] = [
-    {
-      id: 1,
-      title: "Learn React",
-      isCompleted: false,
-    },
-  ];
+function App(){
+  const [taskName, setTaskName] = useState("");
+  const [tasks, setTasks] = useState<Task[]>([
+  {
+    id: 1,
+    title: "Learn React",
+    isCompleted: true,
+    priority: "p1"
+  }
+  ]);
+
+  const addTask = () => {
+    console.info(taskName);
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(),
+        title: taskName,
+        isCompleted: false
+      }
+    ])
+  };
+
   return (
     <div>
       <h1>Tasks</h1>
+      <label htmlFor="input-task"></label>
+      <input id="input-task" value={taskName} onChange={(event) => setTaskName(event.target.value) } />
+      <button onClick={addTask}>Add</button>
       <ul>
-        {task.map((task) => {
-          return <li key={task.id}>{task.title}</li>;
+        {tasks.map((task) => {
+          return <li key={task.id}>{task.title}</li>
         })}
       </ul>
     </div>
-  );
+  )
 }
 
 export default App;
