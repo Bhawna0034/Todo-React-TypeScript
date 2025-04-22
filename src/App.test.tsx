c3ee0b1ccb473379a9e40f12f9b5ca5109868f19
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-22 17:05:47 +0530

    TDD

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,32 +1,46 @@
-import {describe, expect, test } from "vitest";
-import {render, screen, waitFor} from "@testing-library/react"
+import { describe, expect, test } from "vitest";
 import userEvent from "@testing-library/user-event"
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 
+describe("App", () => {
+  test("should clear the input field after adding the task", async() => {
+    const user = userEvent.setup();
+    render(<App />);
 
+    const input = screen.getByRole('textbox', {name: 'Add Task:'});
+    const button = screen.getByRole('button', {name: 'Add'});
 
-describe('App', () => {
-    test('should render input field and add button', () => {
-        render(<App/>);
+    await user.type(input, 'New Task');
+    await user.click(button);
 
-        const input = screen.getByRole('textbox', {name: 'Add Task:'});
-        const button = screen.getByRole('button', {name: 'Add'});
-
-        expect(input).toBeInTheDocument();
-        expect(button).toBeInTheDocument();
+    await waitFor(() => {
+        expect(input).toHaveValue('');
     });
+  });
 
-    test('should add task to list when add button is clicked', async() => {
-        const user = userEvent.setup();
-        render(<App />);
-        const input = screen.getByRole('textbox', {name: 'Add Task:'});
-        const button = screen.getByRole('button', {name: 'Add'});
-        
-        await user.type(input, 'New Task');
-        await user.click(button);
+  test("should not add an empty task", async() => {
+    const user = userEvent.setup();
+    render(<App/>);
+    
+    const input = screen.getByRole('textbox', {name: 'Add Task:'});
+    const button = screen.getByRole('button', {name: 'Add'});
 
-        await waitFor(() => {
-            expect(screen.getByText('New Task')).toBeInTheDocument();
-        });
+    await user.type(input, ' ');
+    await user.click(button);
+
+    await waitFor(() => {
+        expect(screen.queryAllByRole('listitem')).toHaveLength(0);
     });
+  });
+  test("should add a task by pressing the enter key", async() => {
+    const user = userEvent.setup();
+    render(<App />);
+
+    const input = screen.getByRole('textbox', {name: 'Add Task:'});
+    await user.type(input, 'New Task{enter}');
+    await waitFor(() => {
+        expect(screen.queryAllByRole('listitem')).toHaveLength(1);
+    });
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,56 @@ import { useState } from "react";
 
 type Priority = "p1" | "p2" | "p3";
 type Task = {
-  id: number,
-  title: string,
-  isCompleted: boolean,
-  priority?: Priority
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
 };
 
-function App(){
+function App() {
   const [taskName, setTaskName] = useState("");
-  const [tasks, setTasks] = useState<Task[]>([
-  {
-    id: 1,
-    title: "Learn React",
-    isCompleted: true,
-    priority: "p1"
-  }
-  ]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
+  const inputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if(event.key === 'Enter'){
+      addTask();
+    }
+  }
   const addTask = () => {
+    const trimmedTaskName = taskName.trim();
+    if (!trimmedTaskName) {
+      return;
+    }
     console.info(taskName);
     setTasks([
       ...tasks,
       {
         id: new Date().getTime(),
         title: taskName,
-        isCompleted: false
-      }
-    ])
+        isCompleted: false,
+      },
+    ]);
+    setTaskName("");
   };
 
   return (
     <div>
       <h1>Tasks</h1>
       <label htmlFor="input-task">Add Task:</label>
-      <input id="input-task" value={taskName} onChange={(event) => setTaskName(event.target.value) } />
+      <input
+        id="input-task"
+        value={taskName}
+        onChange={(event) => setTaskName(event.target.value)}
+        onKeyDown={inputKeyDown}
+      />
       <button onClick={addTask}>Add</button>
       <ul>
         {tasks.map((task) => {
-          return <li key={task.id}>{task.title}</li>
+          return <li key={task.id}>{task.title}</li>;
         })}
       </ul>
     </div>
-  )
+  );
 }
 
 export default App;
