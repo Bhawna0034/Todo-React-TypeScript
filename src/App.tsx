c4ad2210dfba03ca7d0297089c1bbec2bf66bae8
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-22 12:27:14 +0530

    Reading Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 // import './App.css'
 
+type Priority = "p1" | "p2" | "p3";
+type Task = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+  priority?: Priority;
+};
+
 function App() {
-  return <div>Hello World!</div>
+  const task: Task[] = [
+    {
+      id: 1,
+      title: "Learn React",
+      isCompleted: false,
+    },
+  ];
+  return (
+    <div>
+      <h1>Tasks</h1>
+      <ul>
+        {task.map((task) => {
+          return <li key={task.id}>{task.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
